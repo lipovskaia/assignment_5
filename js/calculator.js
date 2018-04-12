e8d28f127fc4013b89073d4c479750ee347c3dea
lipovskaia <natalia.lipovskaia@gmail.com>
2018-04-11 21:43:44 -0700

    new

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,27 @@
 /*eslint-env browser*/
-window.onload = function() {
+
+
+
     
-    
-    
+
+"use strict";
+var $ = function(id) { return document.getElementById(id)};
+document.getElementByTagName("input")[i].addEventListener("click", myFunction);
+
+function myFunction(){
+    var number = document.getElementByName("button");
+    if (number.hasAttribute("value")){
+        $("result").nodeValue = number.getAttribute("value");
+    }
     
 }
+
+       
+
+    
+    
+    
+    
+    
+    
+    
